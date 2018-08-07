bfddec2e90712c3fc6048fc19d6bfd3fd2086e15
GitMamut <vatier@gazeta.pl>
2018-08-07 21:29:01 +0200

    Add basic reading requests from a sensor specified in URL

--- src/main/java/mintfrost/cloud/Application.java
+++ src/main/java/mintfrost/cloud/Application.java
@@ -2,24 +2,39 @@ package mintfrost.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
     @RequestMapping("/")
     public String home() {
         return "Hello Docker World";
     }
 
-    @RequestMapping("/test")
-    public String test(@RequestParam("user") String user) {
-        return "Testing 1,2,3...<br/>Hello " + user;
-    }
+    @RequestMapping("/sensors/{sensorEndpoint}")
+    public String sensors(@PathVariable(value="sensorEndpoint") String sensorEndpoint) {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        RestTemplate rest = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        headers.add("Accept", "*/*");
+
+
+        HttpEntity<String> requestEntity = new HttpEntity<>("", headers);
+        ResponseEntity<String> responseEntity = rest.exchange("http://192.168.1.27:8080/" + sensorEndpoint, HttpMethod.GET, requestEntity, String.class);
+
+        return responseEntity.getBody();
     }
 }
